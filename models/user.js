a0dcb03d4279c6ceab23b3849ea4d1e4af206727
lucasiqueira86 <lucasprogramador86@gmail.com>
2020-08-17 14:11:40 -0300

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,20 @@
+//Para declarar uma classe a primeira letra tem que ser maicusla
+class User{                
+
+constructor(name,gender,birth,country,email,password,photo,admin){
+
+    this.name = name;
+    this.gender = gender;
+    this.birth = birth;  
+    this.country = country;
+    this.email = email;
+    this.password = password;
+    this.photo = photo;
+    this.admin = admin;
+
+
+
+  }
+
+
+} 
